01597e171656976d4aff2ad94acd272e6f1c6ba6
Milena <milena.devweb@gmail.com>
2025-01-09 04:58:55 -0300

    Foi desenvolvida o SSO com github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Micro Auth SSO - Dashboard</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -20,6 +20,12 @@
         @endif
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="flex flex-row items-center justify-end py-2 px-6">
+            <a href="/auth/github/redirect">Autenticar com GitHub</a>
+        </div>
+        <div class="flex flex-row items-center justify-end px-6">
+            <a href="/auth/google/redirect">Autenticar com Google</a>
+        </div>
         <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
             <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,59 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/github/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/github/callback', function () {
+    $githubUser = Socialite::driver('github')->user();
+
+    $user = User::updateOrCreate([
+        'github_id' => $githubUser->id,
+    ], [
+        'name' => $githubUser->nickname,
+        'email' => $githubUser->email,
+        'github_token' => $githubUser->token,
+        'github_refresh_token' => $githubUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/logged');
+});
+
+Route::get('/auth/google/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+    // dd($googleUser);
+
+    $user = User::updateOrCreate([
+        'github_id' => $googleUser->id,
+    ], [
+        'name' => $googleUser->nickname,
+        'email' => $googleUser->email,
+        'github_token' => $googleUser->token,
+        'github_refresh_token' => $googleUser->refreshToken,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/logged');
+});
+
+Route::get('/logged', function() {
+
+    dd(Auth::user());
+
 });
